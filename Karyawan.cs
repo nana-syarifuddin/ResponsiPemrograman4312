ce44d50a91ea8684c83123e8aa0783cde9d59d8c
nana-syarifuddin <nana.informatika@students.amikom.ac.id>
2022-04-19 16:16:52 +0700

    Responsi Pemrograman

---
+++ Karyawan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4312
+{
+    class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public Karyawan(string nik, string nama, int gaji)
+        {
+            Nik = nik;
+            Nama = nama;
+
+            if (gaji < 0)
+            {
+                GajiBulanan = 0;
+            }
+            else
+            {
+                GajiBulanan = gaji;
+            }
+        }
+
+        public void infoKaryawan()
+        {
+            Console.WriteLine("{0} \t {1} \t {2}", Nik, Nama, GajiBulanan);
+        }
+
+        public void kenaikanGaji()
+        {
+            int gaji = 0;
+            gaji = (10 * GajiBulanan) / 100;
+            GajiBulanan = GajiBulanan + Convert.ToInt32(gaji);
+            Console.WriteLine("{0} \t {1} \t {2}", Nik, Nama, GajiBulanan);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4312
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("190302123", "Paijo", 3000000);
+            Karyawan karyawan2 = new Karyawan("190302124", "Jono", 2000000);
+
+            Console.WriteLine("No. Nik \t Nama \t Gaji Bulanan");
+            Console.WriteLine("-------------------------------------");
+            Console.Write("1. ");
+            karyawan1.infoKaryawan();
+            Console.Write("2. ");
+            karyawan2.infoKaryawan();
+
+            Console.WriteLine("\nAsyiiiiik kenaikan gaji 10%!!");
+
+            Console.WriteLine("\nNo. Nik \t Nama \t Gaji Bulanan");
+            Console.WriteLine("--------------------------------------");
+            Console.Write("1. ");
+            karyawan1.kenaikanGaji();
+            Console.Write("2. ");
+            karyawan2.kenaikanGaji();
+            
+            Console.ReadKey();
+        }
+    }
+}
